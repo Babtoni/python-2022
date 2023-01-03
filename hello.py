b7bb105b3c841728f0e14c455569a0b957217031
Oni Babatunde <babatundeoni80@gmail.com>
2023-01-03 10:18:26 +0100

    Initial Commit

---
+++ hello.py
@@ -0,0 +1,29 @@
+import os
+os.system('clear')
+
+
+############################
+# DATA TYPES
+# Strings
+# Numbers
+# Lists
+# Tuples
+# Dictionaries
+# Boolean
+#############################
+
+
+
+first_name = 'Oni'
+age = 41
+fav_pizza = {
+	'Oni': 'Pepperoni',
+	'David': 'Mushroom',
+	'Gbolahan': 'Cheese'
+	}
+name = True
+
+
+print (name)
+
+
